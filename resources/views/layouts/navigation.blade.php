12912ccbb413d4c3a176f49db7421fd4a81ca05a
Maxim Fateyev <maxim.f@rocketfirm.com>
2024-07-13 11:39:04 +0500

    initial commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,6 @@
-
+@if(isset($config->address)) @section( 'chinaaddress', $config->address ) @endif
+@if(isset($config->title_text)) @section( 'title_text', $config->title_text ) @endif
+@if(isset($config->address_two)) @section( 'address_two', $config->address_two ) @endif
     <nav x-data="{ open: false }" class="bg-[#313131] border-b border-gray-100">
         <!-- Primary Navigation Menu -->
 
